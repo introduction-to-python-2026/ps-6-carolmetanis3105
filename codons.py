c5433d93724a76cc1db6e88a645b7cf17eccb8d4
carolmetanis3105 <carol.metanis@gmail.com>
2025-11-28 23:19:05 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -1,4 +1,20 @@
 def create_codon_dict(file_path):
-    pass # Replace the pass with your code
+    
+    codon_dict = {}
+    
+    with open(file_path, 'r') as f:
+        for line in f:
+            line = line.strip()
+            # skip empty lines or comments
+            if not line or line.startswith("#"):
+                continue
+            
+            parts = line.split()
+            codon = parts[0]
+            amino_acid = parts[1]
+            
+            codon_dict[codon] = amino_acid
+    
+    return codon_dict
 
 
